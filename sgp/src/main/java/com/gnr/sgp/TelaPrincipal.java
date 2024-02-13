3d4e397e106b23acb60494ad240843e3a2d22181
Guilherme Rodrigues <guizaumnr@gmail.com>
2024-02-13 08:29:46 -0300

    Alterando versao do sistema

--- sgp/src/main/java/com/gnr/sgp/TelaPrincipal.java
+++ sgp/src/main/java/com/gnr/sgp/TelaPrincipal.java
@@ -61,7 +61,7 @@ import javax.swing.Timer;
  */
 public class TelaPrincipal extends javax.swing.JFrame {
 
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "1.0.1";
 
     Date dataSistema = new Date();
     SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy");
